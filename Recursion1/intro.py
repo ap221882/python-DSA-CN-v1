8efa77cc53bd5cd6ede51bef6e257f562248f707
Ajay <ap221882@gmail.com>
2022-03-02 20:42:28 +0530

    till-7

--- Recursion1/intro.py
+++ Recursion1/intro.py
@@ -35,6 +35,40 @@ def pow_n(x, n):
     return x*pow_n(x, n-1)
 
 
+# x = int((input()))
+# n = int((input()))
+# print(pow_n(x, n))
+
+
+# 6
+def print_1_to_n(n):
+    if n == 0:
+        return
+    print_1_to_n(n-1)
+    print(n)
+    return
+
+
+# x = int((input()))
+# print_1_to_n(x)
+
+
+def print_n_to_1(n):
+    if n == 0:
+        return
+    print(n)
+    print_n_to_1(n-1)
+    return
+
+
 x = int((input()))
-n = int((input()))
-print(pow_n(x, n))
+print_n_to_1(x)
+
+
+# 8
+# nth Fibonacci sum -- 1,1,2,3,5,...
+# fib(n)=fib(n-1)+fib(n-2)
+def fib(n):
+    if n == 1:
+        return 1
+    fib_n_1 = fib(n-1)
